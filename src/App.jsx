a9e7cd952f5aee60615d8b3360fa904534ae4e13
Qussai Khaddour <qusai.khadour7@gmail.com>
2025-05-31 18:54:27 +0200

    scroll up update

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,29 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
-import Project1 from './components/Project1'; // <-- added
+import Project1 from './components/Project1';
 import Project2 from './components/Project2';
 import Project3 from './components/Project3';
-
+import ScrollToTop from './components/ScrollToTop'; // ✅ Import the component!
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={
-        <>
-          <Hero />
-          <About />
-          <Projects />
-          <Footer />
-        </>
-      } />
-      <Route path="/project1" element={<Project1 />} />
-      <Route path="/project2" element={<Project2 />} />
-      <Route path="/project3" element={<Project3 />} />
-    </Routes>
+    <>
+      <ScrollToTop /> {/* ✅ This makes sure you always start at the top */}
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Hero />
+            <About />
+            <Projects />
+            <Footer />
+          </>
+        } />
+        <Route path="/project1" element={<Project1 />} />
+        <Route path="/project2" element={<Project2 />} />
+        <Route path="/project3" element={<Project3 />} />
+      </Routes>
+    </>
   );
 }
 
